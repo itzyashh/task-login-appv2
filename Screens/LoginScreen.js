fe5e44496f9b930b7bf31b8988b10f72d7f5bcd1
itzyashh <yashjadhav1411@rocketmail.com>
2022-04-20 13:29:49 +0530

    Update LoginScreen.js

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -5,68 +5,68 @@ import tw from 'twrnc';
 import axios from 'axios'
 import { useState } from 'react';
 import FlatButton from '../shared/button';
-import { 
+import {
   Inter_300Light,
   Inter_400Regular,
   Inter_500Medium,
   Inter_600SemiBold,
   Inter_700Bold,
-  Inter_900Black 
+  Inter_900Black
 } from '@expo-google-fonts/inter'
 import { useFonts } from 'expo-font'
 import AppLoading from 'expo-app-loading';
- 
+
 
 const LoginScreen = () => {
 
   const [userEmail, setUserEmail] = useState('')
   const [password, setPassword] = useState("")
-  const submit =()=> {
+  const submit = () => {
 
     axios({
-          method: 'POST',
-          url: 'https://reqres.in/api/login',
-          data: {
-            email: userEmail,
-            password: password
-          },
-          headers: {
-            'Accept': '/'
-          }
-        })
-          .then((response) => {
-            const success = JSON.stringify(response.data)
-            Alert.alert(
-              "Success",
-              `${success}`,
-              [
-                {
-                  text: "Go to DashBoard",
-                  onPress: () => navigation.navigate('Dashboard'),
-            
-                }
-              ]
-            );
-            console.log(JSON.stringify(response.data));
-      
-          })
-          .catch((response) => {
-            console.log(userEmail);
-            console.log(password);
-            Alert.alert(
-              "Failed",
-              `${response}`,
-              [
-                {
-                  text: "Try Again",
-                  onPress: () => navigation.navigate('Login'),
-            
-                }
-              ]
-            );
-          })
+      method: 'POST',
+      url: 'https://reqres.in/api/login',
+      data: {
+        email: userEmail,
+        password: password
+      },
+      headers: {
+        'Accept': '/'
+      }
+    })
+      .then((response) => {
+        const success = JSON.stringify(response.data)
+        Alert.alert(
+          "Success",
+          `${success}`,
+          [
+            {
+              text: "Go to DashBoard",
+              onPress: () => navigation.navigate('Dashboard'),
 
-   
+            }
+          ]
+        );
+        console.log(JSON.stringify(response.data));
+
+      })
+      .catch((response) => {
+        console.log(userEmail);
+        console.log(password);
+        Alert.alert(
+          "Failed",
+          `${response}`,
+          [
+            {
+              text: "Try Again",
+              onPress: () => navigation.navigate('Login'),
+
+            }
+          ]
+        );
+      })
+
+
   }
 
 
@@ -76,34 +76,34 @@ const LoginScreen = () => {
     Inter_500Medium,
     Inter_600SemiBold,
     Inter_700Bold,
-    Inter_900Black 
+    Inter_900Black
   });
 
-  if(!fontsLoad){
+  if (!fontsLoad) {
     return <AppLoading />
   }
 
   const navigation = useNavigation();
   // const [isShowingText, setIsShowingText] = useState(true);
-  
+
   return (
     <View style={styles.mainBlock}>
       <Text style={styles.textTitle} >Welcome!</Text>
-     
-        <Text style={styles.textNormal} >Email</Text>
-        <TextInput style={tw`mt-1 mx-7 -mb-7 p-4 rounded-lg  bg-gray-100 text-gray-600 `} placeholder='Enter Email' autoCapitalize='none' value={userEmail} 
-        onChangeText={(actualData)=>setUserEmail(actualData)}
-          
-        ></TextInput>
 
-        <Text style={styles.textNormal}>Password</Text>
-        <TextInput style={tw`mb-2 mt-1 mx-7  p-4 rounded-lg  bg-gray-100 text-gray-600`} placeholder='Enter Password' secureTextEntry={true} 
+      <Text style={styles.textNormal} >Email</Text>
+      <TextInput style={tw`mt-1 mx-7 -mb-7 p-4 rounded-lg  bg-gray-100 text-gray-600 `} placeholder='Enter Email' autoCapitalize='none' value={userEmail}
+        onChangeText={(actualData) => setUserEmail(actualData)}
+
+      ></TextInput>
+
+      <Text style={styles.textNormal}>Password</Text>
+      <TextInput style={tw`mb-2 mt-1 mx-7  p-4 rounded-lg  bg-gray-100 text-gray-600`} placeholder='Enter Password' secureTextEntry={true}
         value={password}
-        onChangeText={(actualData)=>setPassword(actualData)}
-        ></TextInput>
-       
-        <FlatButton text="Login" onPress={() => {submit()}}/>
-      
+        onChangeText={(actualData) => setPassword(actualData)}
+      ></TextInput>
+
+      <FlatButton text="Login" onPress={() => { submit() }} />
+
     </View>
   )
 }
@@ -112,11 +112,11 @@ const styles = StyleSheet.create({
     fontSize: 40,
     fontFamily: 'Inter_400Regular'
   },
-  mainBlock:{
+  mainBlock: {
     fontFamily: 'Inter_400Regular',
-    backgroundColor:'white',
+    backgroundColor: 'white',
     // paddingVertical:165,
-    height:"100%"
+    height: "100%"
   },
   textTitle: {
     marginTop: "30%",
@@ -134,8 +134,8 @@ const styles = StyleSheet.create({
     backgroundColor: 'black',
   },
   textNormal: {
-    marginHorizontal:40,
-    marginTop:40,
+    marginHorizontal: 40,
+    marginTop: 40,
     color: 'gray',
     fontFamily: 'Inter_400Regular'
   }
